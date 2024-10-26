51d2ef27224647280192df040d5b5872e0c4d3b6
ncrekai <nrekai@my.centennialcollege.ca>
2024-10-26 13:56:20 -0400

    Controller/User good

--- server/controller/User.js
+++ server/controller/User.js
@@ -4,44 +4,29 @@ const create = async(req,res) => {
     const user = new User(req.body)
     try{
         await user.save()
-        return res.json({
-            message:"Added Successfully"
-        })
-    }
-    catch(err)
-    {
-        return res.json({
-            error:"Error Message"
-        })
+        return res.json({ message:"Added Successfully" })
+    } catch(err) {
+        return res.json({ error: "I broke (model/user/create)" })
     }
 }
-//update
+
 const list = async(req,res)=>{
     try{
         let users = await User.find().select('name email updated created');
         res.json(users);
-    }
-    catch(err){
-        return res.json({
-            error:"Error Message"
-        })
+    } catch(err) {
+        return res.json({ error: "I broke (model/user/list)" })
     }
 }
 
 const userById = async(req,res,next,id)=>{
     try{
         let user = await User.findById(id);
-        if(!user)
-            return res.json({
-        error:"User doesn't exist"
-        })
-    req.profile = user
-    next()
-    }
-    catch(err){
-        return res.json({
-            error:"Error Message"
-        })
+        if (!user) return res.json({ error:"User doesn't exist" })
+        req.profile = user
+        next()
+    } catch(err) {
+        return res.json({ error: "I broke (model/user/userById)" })
     }
 }
 
@@ -50,6 +35,7 @@ const read = (req,res) =>{
     req.profile.salt = undefined
     return res.json(req.profile)
 }
+
 const update = async(req,res) => {
     try{
         let user = req.profile
@@ -59,14 +45,11 @@ const update = async(req,res) => {
         user.hashed_password = undefined
         user.salt  = undefined
         res.json(user)
-    }
-    catch(err)
-    {
-    return res.json({
-            error:"Error Message"
-        })   
+    } catch(err) {
+        return res.json({ error: "I broke (model/user/update)" })
     }
 }
+
 const remove = async(req,res) => {
     try{
         let user = req.profile
@@ -74,12 +57,9 @@ const remove = async(req,res) => {
         deletedUser.hashed_password = undefined
         deletedUser.salt = undefined
         res.json(deletedUser)
-    }
-    catch(err)
-    {
-    return res.json({
-            error:"Error Message"
-        })   
+    } catch(err) {
+        return res.json({ error: "I broke (model/user/remove)" })
     }
 }
-module.exports = {create, userById,read,list,remove,update}
+
+module.exports = {create, list, userById, read, update, remove}
